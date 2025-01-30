bd53869cce64bbcdd09057749d0e548e8b4da395
Miroslav Tzarev <mtzarev@gmail.com>
2025-01-30 11:32:32 +0200

    use only useRef hook

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,17 +1,41 @@
-import { useState, useEffect, useRef } from "react";
+import { useRef } from "react";
 function MyComponent () {
-    const [number, setNumber] = useState(0)
-    useEffect (()=>{
-        
-    })
-    function handleClick(){
-        setNumber(n=>n+1);
+   
+  
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+    const  inputRef3 = useRef(null);
+    
+        function handleClick1(){
+        inputRef1.current.focus();
+        inputRef1.current.style.backgroundColor = "green";
+        inputRef2.current.style.backgroundColor = ""
+        inputRef3.current.style.backgroundColor = ""
     }
+    function handleClick2(){
+        inputRef2.current.focus();
+        inputRef1.current.style.backgroundColor = "";
+        inputRef2.current.style.backgroundColor = "green"
+        inputRef3.current.style.backgroundColor = ""
+    }
+    function handleClick3(){
+        inputRef3.current.focus();
+        inputRef1.current.style.backgroundColor = "";
+        inputRef2.current.style.backgroundColor = ""
+        inputRef3.current.style.backgroundColor = "green"
+    }
+
     return (
             <>
-            <h1>Number: {number}</h1>
-            <button onClick={handleClick}>Click Me!</button>
             
+            <button onClick={handleClick1}>Click Me 1!</button>
+            <input ref={inputRef1} />
+            <hr />
+            <button onClick={handleClick2}>Click Me 2!</button>
+            <input ref={inputRef2} />
+            <hr />
+            <button onClick={handleClick3}>Click Me 3!</button>
+            <input ref={inputRef3} />
             </>
     );
 }
